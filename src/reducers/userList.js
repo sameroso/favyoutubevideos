522bea6664e8f9a95ba341af691c2cb1b9b761b9
Samer <samerkkr@gmail.com>
2020-04-23 15:02:41 -0300

    more changes

--- src/reducers/userList.js
+++ src/reducers/userList.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 export default (state = {} , action) => {
     switch(action.type) {
         case 'USER_DATA':
-            return {...state, ...action.payload};
+            return {...state, [action.payload.ID]:action.payload};
         case 'VIDEO_DELETE':
             console.log("oi")
             return _.omit(state, action.payload);
