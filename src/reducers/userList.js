1df65f5081e984018075f3b21ec530deca99bc33
Samer <samerkkr@gmail.com>
2020-04-23 15:17:27 -0300

    test

--- src/reducers/userList.js
+++ src/reducers/userList.js
@@ -3,10 +3,9 @@ import _ from 'lodash';
 export default (state = {} , action) => {
     switch(action.type) {
         case 'USER_DATA':
-            return {...state, [action.payload.ID]:action.payload};
+            return {...state, ..._.mapKeys(action.payload,'ID')}
         case 'VIDEO_DELETE':
-            console.log("oi")
-            return _.omit(state, action.payload);
+            return _.omit(state, action.payload)
         default:
             return state;
     }
